62c6cc7bac0556f4940313ac4c3a51b4e4cdb220
DevFortunato <57877156+MovieMaker93@users.noreply.github.com>
2024-08-09 20:02:50 +0200

    Update .startup.sh

--- .startup.sh
+++ .startup.sh
@@ -6,7 +6,7 @@ install_on_ubuntu() {
     if which nix; then
         echo 'Nix is already installed'
     else
-        sh <(curl -L https://nixos.org/nix/install) --no-daemon --init none
+        sh <(curl -L https://nixos.org/nix/install) --no-daemon
     fi
 }
 
